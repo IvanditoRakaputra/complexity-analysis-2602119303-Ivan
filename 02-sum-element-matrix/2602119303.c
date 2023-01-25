b4e93ba8157b7de420cafb46f09492cdc85719d5
IvanditoRakaputra <114067618+IvanditoRakaputra@users.noreply.github.com>
2023-01-25 21:33:02 +0700

    Add files via upload

---
+++ 02-sum-element-matrix/2602119303.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int sumElementMAtrix(int* matrix,int M, int N){
+    int sum = 0;
+    for (int m=0; m<M; m++){
+        for (int n=0; n<N; n++){
+            sum  += matrix(M*m)
+        }
+    }
+
+
+}
